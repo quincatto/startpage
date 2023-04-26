e7d9215648f22a71fc147ef1b5327fb649d773aa
Quin Catherine <quinraines@gmail.com>
2023-04-26 10:19:14 +0100

    Add shortcuts

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,17 @@
-import { useState } from 'react'
+import { ReactNode, useState } from 'react'
+import { FaGithub, FaReddit, FaTwitter, FaYoutube } from 'react-icons/fa'
+import { RiMovie2Line } from 'react-icons/ri'
 import './App.css'
 import Clock from './components/clock'
 import Greeting from './components/greeting'
 import Shortcut from './components/shortcut'
 
 function App() {
+  const twitter: ReactNode[] = ['https://twitter.com/', <FaTwitter />, 'Twitter'];
+  const reddit: ReactNode[] = ['https://reddit.com/', <FaReddit />, 'Reddit'];
+  const youtube: ReactNode[] = ['https://youtube.com/', <FaYoutube />, 'YouTube'];
+  const github: ReactNode[] = ['https://github.com/', <FaGithub />, 'Github'];
+  const pahe: ReactNode[] = ['https://pahe.li/', <RiMovie2Line />, 'Pahe'];
 
 
   return (
@@ -21,8 +28,12 @@ function App() {
         <Greeting />
       </div>
       <div className='flex items-center justify-center absolute h-screen w-screen top-0 left-0 gap-20'>
-        <div className='flex flex-col items-center justify-center h-1/2 bg-purple-1000 rounded-2xl w-1/3 shadow-xl'>
-          <Shortcut />
+        <div className='flex h-1/2 bg-purple-1000 rounded-2xl w-1/3 shadow-xl'>
+          <Shortcut link={twitter}/>
+          <Shortcut link={reddit}/>
+          <Shortcut link={youtube}/>
+          <Shortcut link={github}/>
+          <Shortcut link={pahe}/>
         </div>
         <div className='flex flex-col items-center justify-center h-1/2 bg-purple-1000 rounded-2xl w-1/3 shadow-xl'>
         </div>
--- src/components/shortcut.tsx
+++ src/components/shortcut.tsx
@@ -1,13 +1,19 @@
-import { FaTwitter } from 'react-icons/fa';
+import { ReactNode } from "react";
 
-export default function Shortcut() {
+interface ShortcutProps {
+    link: ReactNode[];
+}
+
+export default function Shortcut({ link }: ShortcutProps) {
 
     return (
         <>
-            <a href="https://twitter.com/" className='flex flex-col items-center border border-surface0 bg-surface0 p-4 rounded'>
-                <FaTwitter />
-                <h2>Twitter</h2>
-            </a>
+            <div className="mx-2 mt-16">
+                <a href={link[0] as string} className='flex flex-col items-center border border-surface0 bg-surface0 p-4 rounded mx-5 w-20'>
+                    {link[1]}
+                    <h2>{link[2]}</h2>
+                </a>
+            </div>
         </>
     )
 }
