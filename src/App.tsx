e48cff9e62d3d2bd87ebf37c6008f738c4456944
quincatto <quinraines@gmail.com>
2023-04-26 11:52:54 +0100

    Adding shortcuts

--- src/App.tsx
+++ src/App.tsx
@@ -16,29 +16,29 @@ function App() {
 
   return (
     <>
-    <div className="background">
-    </div>
-    <div className='foreground'>
-      <div className="relative h-32">
-        <div className='font-mono absolute mx-auto text-center left-0 right-0 top-10 text-xl text-shadow'>
-          <Clock />
+      <div className="background">
+      </div>
+      <div className='foreground'>
+        <div className="relative h-32">
+          <div className='font-mono absolute mx-auto text-center left-0 right-0 top-10 text-xl text-shadow'>
+            <Clock />
+          </div>
+        </div>
+        <div className="text-center mt-10">
+          <Greeting />
+        </div>
+        <div className='flex items-center justify-center absolute h-screen w-screen top-0 left-0 gap-20'>
+          <div className='grid grid-cols-5 grid-rows-3 h-1/2 bg-purple-1000 rounded-2xl w-1/3 shadow-xl'>
+            <Shortcut link={twitter} />
+            <Shortcut link={reddit} />
+            <Shortcut link={youtube} />
+            <Shortcut link={github} />
+            <Shortcut link={pahe} />
+          </div>
+          <div className='grid grid-cols-5 grid-rows-3 h-1/2 bg-purple-1000 rounded-2xl w-1/3 shadow-xl'>
+          </div>
         </div>
       </div>
-      <div className="text-center mt-10">
-        <Greeting />
-      </div>
-      <div className='flex items-center justify-center absolute h-screen w-screen top-0 left-0 gap-20'>
-        <div className='flex h-1/2 bg-purple-1000 rounded-2xl w-1/3 shadow-xl'>
-          <Shortcut link={twitter}/>
-          <Shortcut link={reddit}/>
-          <Shortcut link={youtube}/>
-          <Shortcut link={github}/>
-          <Shortcut link={pahe}/>
-        </div>
-        <div className='flex flex-col items-center justify-center h-1/2 bg-purple-1000 rounded-2xl w-1/3 shadow-xl'>
-        </div>
-      </div>
-    </div>
     </>
   )
 }
--- src/components/shortcut.tsx
+++ src/components/shortcut.tsx
@@ -8,7 +8,7 @@ export default function Shortcut({ link }: ShortcutProps) {
 
     return (
         <>
-            <div className="mx-2 mt-16">
+            <div className="mx-2 mt-16 col-span-1">
                 <a href={link[0] as string} className='flex flex-col items-center border border-surface0 bg-surface0 p-4 rounded mx-5 w-20'>
                     {link[1]}
                     <h2>{link[2]}</h2>
